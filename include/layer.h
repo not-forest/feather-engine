a196265652d26f7676f956a30aeffd1435815648
Notforest <notforest69@gmail.com>
2024-10-13 20:12:34 +0200

    Added header to define layers, which are user defined scheduled handler functions.

---
+++ include/layer.h
@@ -0,0 +1,44 @@
+/**************************************************************************************************
+ *  File: layer.h
+ *  Desc: Defines layers API, which are user defined functions that define main application functionality
+ *  and being scheduled during game update stage.
+ **************************************************************************************************
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ *
+ * */
+
+#pragma once
+
+#ifndef FEATHER_LAYER_H
+#define FEATHER_LAYER_H
+
+#include <tllist.h>
+
+/* 
+ *  @brief - each layer is a user defined function that will be scheduled during update phase.
+ * */
+typedef void (*fLayer)(void *anyUserData);
+
+/* 
+ *  @brief - list of layer functions.
+ * */
+typedef tll(fLayer) tLayerList;
+
+
+#endif
