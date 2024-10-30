67cc1c74277b88e8e535339b32b77199e2a6e057
Notforest <notforest69@gmail.com>
2024-10-30 16:11:07 +0100

    Added minimal glsl shaders to use with opengl.

---
+++ shaders/rect.frag.glsl
@@ -0,0 +1,38 @@
+/**************************************************************************************************
+ *  File: rect.frag.glsl
+ *  Desc: Samples the texture based on obtained input coordinates. Falls back to base color otherwise.
+ *  Used on all created Rects.
+ **************************************************************************************************
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ *
+ * */
+
+#version 330 core
+
+out vec4 FragColor;
+
+in vec2 iTexCoord;              // Texture coordinates from vertex shader
+uniform vec3 uColor;            // Color for the rectangle
+uniform sampler2D uTexture;
+
+void main() {
+    // Mix color and texture (Blend factor can be adjusted if needed)
+    vec4 textureColor = texture(uTexture, TexCoord);
+    FragColor = textureColor * vec4(uColor, 1.0);
+}
