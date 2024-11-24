d8240fc5152861c8ad4fd955bfd400811c2135ee
Notforest <notforest69@gmail.com>
2024-11-24 16:06:53 +0100

    Now layer is a set of function pointer and a priority.

--- include/layer.h
+++ include/layer.h
@@ -28,17 +28,26 @@
 #ifndef FEATHER_LAYER_H
 #define FEATHER_LAYER_H
 
+#include <stdint.h>
 #include <tllist.h>
 
 /* 
  *  @brief - each layer is a user defined function that will be scheduled during update phase.
+ *
+ *  @fRun - actual pointer to the layer function.
+ *  @iPriority - priority of which this layer is executed. Negative values are only executed on 
+ *  initialization according to the value they have, i.e (-2) will be runned only two times. Negative 
+ *  values also have the highest priority.
  * */
-typedef void (*fLayer)(void *tRun);
+typedef struct {
+    void (*fRun)(void *tRun);
+    int iPriority;  
+} tLayer;
 
 /* 
  *  @brief - list of layer functions.
  * */
-typedef tll(fLayer) tLayerList;
+typedef tll(tLayer) tLayerList;
 
 
 #endif
