074a51af776711f0db66a16755ab2cb30fb2a4f8
not-forest <notforest69@gmail.com>
2025-01-06 16:21:46 +0100

    Added framing support. Added animation support.

--- include/rect.h
+++ include/rect.h
@@ -1,7 +1,7 @@
 /**************************************************************************************************
  *  File: rect.h
  *  Desc: Rects are engine's 2D primitives for drawing content onto the screen. They define bounding 
- *  box for each object or entity and apply coloring or texture.
+ *  box for each object or entity and apply coloring or texture. Frames allow to animate the rect.
  **************************************************************************************************
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -34,11 +34,34 @@
 #include <intrinsics.h>
 
 /* 
+ *  @brief - helper framing structure to indexate the surface of the rect.
+ * */
+typedef struct {
+    uint8_t uIdx; 
+    uint32_t uWidth, uHeight;
+} tFrame;
+
+/* 
+ *  @brief - animation structure, which stores a set of frame IDs.
+ *
+ *  @uCurrent - currently rendered frame
+ *  @uFrames - list of all frames within a single animation.
+ * */
+typedef struct {
+    uint8_t uCurrent;
+    tll(uint8_t) uFrames;
+} tAnimation;
+
+/* 
  *  @brief - Rect data type.
  *
  *  @sTexturePath   - path to the texture for the rectangle. A solid color will be used if the texture is NULL.
  *  @tCtx           - 2D context of the rectangle. Mutating it is a proper way of changing it's location, size and rotation. 
  *  @uPriority      - priority for rendering. Higher priorities will be rendered on top of lower ones. 
+ *  @sdlRect        - underlying pointer to SDL rect representation.
+ *  @uAnimationID   - currently running animation.
+ *  @tAnims         - animations appended to the rect.
+ *  @tFr            - frame buffer.
  *
  *  Rects are main boxes for holding information about something that shall be drawn on the screen, 
  *  it's boundaries and coordinates.
@@ -49,11 +72,32 @@ typedef struct {
     tContext2D tCtx;
     uint16_t uPriority;   
     SDL_Rect *sdlRect;
+    tFrame tFr;
+
+    uint16_t uAnimationId;
+    tll(tAnimation) tAnims;
 } tRect;
 
 /* 
  *  @brief - List of drawable rectangles.
  * */
 typedef tll(tRect) tRectList;
+
+/* 
+ *  @brief - choose the frame to draw in the next render cycle.
+ * */
+void vRectFrame(tRect *tRct, uint8_t uIdx);
+
+/* 
+ *  @brief - indexates over the surface of this rect, allowing to use individual frames of different sizes.
+ * */
+void vRectIndexate(tRect *tRct, uint8_t uIdx, uint32_t uWidth, uint32_t uHeight);
+
+/* 
+ *  @brief - append animation to the rectangle
+ *
+ *  Returns an ID of the currently appended animation.
+ * */
+uint16_t vRectAppendAnimation(tRect *tRct, uint8_t *uIdx, uint8_t uInxLength);
 
 #endif
--- src/core/rect.c
+++ src/core/rect.c
@@ -35,10 +35,13 @@
 
 // Initialize a rectangle
 tRect* tInitRect(tRuntime *tRun, tContext2D tCtx, uint16_t uPriority, char* sTexturePath) {
-    tRect rect;
-    rect.tCtx = tCtx;
-    rect.sTexturePath = sTexturePath;
-    rect.uPriority = uPriority;
+    tRect rect = { 
+        .tAnims = tll_init(), 
+        .tCtx = tCtx, 
+        .sTexturePath = sTexturePath, 
+        .uPriority = uPriority, 
+        .tFr.uIdx = 0 
+    };
 
     // Load texture using SDL_image
     SDL_Surface* surface = IMG_Load(sTexturePath);
@@ -46,6 +49,9 @@ tRect* tInitRect(tRuntime *tRun, tContext2D tCtx, uint16_t uPriority, char* sTex
         vFeatherLogError("Unable to load rect texture: %s", IMG_GetError());
         return NULL;
     }
+
+    rect.tFr.uWidth = surface->w; 
+    rect.tFr.uHeight = surface->h;
 
     // Generate SDL_Texture from surface
     SDL_Texture* texture = SDL_CreateTextureFromSurface(tRun->sdlRenderer, surface);
@@ -70,6 +76,72 @@ tRect* tInitRect(tRuntime *tRun, tContext2D tCtx, uint16_t uPriority, char* sTex
     // New higher priority rectangles are pushed to the front
     tll_push_back(tRun->sScene->lRects, rect);
     return (tRect*)tRun->sScene->lRects.tail;
+}
+
+/* 
+ *  @brief - choose the frame to draw in the next render cycle.
+ * */
+void vRectFrame(tRect *tRct, uint8_t uIdx) {
+    tRct->tFr.uIdx = uIdx;
+}
+
+/* 
+ *  @brief - indexates over the surface of this rect, allowing to use individual frames of different sizes.
+ * */
+void vRectIndexate(tRect *tRct, uint8_t uIdx, uint32_t uWidth, uint32_t uHeight) {
+    tRct->tFr.uIdx = uIdx;
+    tRct->tFr.uHeight = uHeight;
+    tRct->tFr.uWidth = uWidth;
+}
+
+/* 
+ *  @brief - Animates the rect according to the currently chosen rect.
+ *
+ *  @tRct           - the rect that shall be animated.
+ *  @uAnimationId   - id of previously appended animations.
+ *  @fSpeed         - amount of ms, before the next frame will appear.
+ *
+ * */
+void vAnimateFrame(tRuntime *tRun, tRect *tRct, uint16_t uAnimationId, float fSpeed) {
+    tRct->uAnimationId = uAnimationId;
+
+    vFeatherSleepThisLayerMs(tRun, fSpeed) {
+        uint16_t uIIdx = 0;
+        tll_foreach(tRct->tAnims, tAnim) {
+            if (uIIdx == uAnimationId) {
+                uint16_t uFIdx = 0;
+                ++tAnim->item.uCurrent;
+                if (tll_length(tAnim->item.uFrames) == tAnim->item.uCurrent)
+                    tAnim->item.uCurrent = 0;
+
+                tll_foreach(tAnim->item.uFrames, tFrame) {
+                    if (uFIdx == tAnim->item.uCurrent)
+                        tRct->tFr.uIdx = tFrame->item;
+                    ++uFIdx;
+                }
+            }
+            uIIdx++;
+        }
+    }
+}
+
+/* 
+ *  @brief - append animation to the rectangle
+ * */
+uint16_t vRectAppendAnimation(tRect *tRct, uint8_t *uIdx, uint8_t uIdxLength) {
+    if (tRct == NULL) {
+        vFeatherLogError("Rect is null. Impossible to append animation.");
+        return 0;
+    }
+
+    tAnimation tAnim = { .uCurrent = 0, .uFrames = tll_init() };
+    
+    for (uint8_t i = 0; i < uIdxLength; ++i)
+        if (uIdx != NULL)
+            tll_push_back(tAnim.uFrames, *(uIdx + i));
+
+    tll_push_back(tRct->tAnims, tAnim);
+    return tll_length(tRct->tAnims) - 1;
 }
 
 /* 
@@ -106,29 +178,37 @@ void vChangeRectTexture(tRuntime* tRun, tRect* tRct, char* sNewTexturePath) {
 
 // Draw the rectangle using SDL renderer
 void vDrawRect(tRuntime *tRun, tRect *rect) {
-    SDL_Renderer* renderer = tRun->sdlRenderer;
+    SDL_Rect srcRect, dstRect;
+    SDL_Point sdlCenter;
+    SDL_Renderer* sdlRend = tRun->sdlRenderer;
     SDL_Texture* texture = (SDL_Texture*)rect->idTextureID;
 
-    SDL_Rect dstRect;
-    dstRect.x = rect->tCtx.fX;
-    dstRect.y = rect->tCtx.fY;
+    int uWidth, uHeight, uColumns;
+    SDL_QueryTexture(texture, NULL, NULL, &uWidth, &uHeight);
 
-    SDL_QueryTexture(texture, NULL, NULL, &dstRect.w, &dstRect.h);
-    dstRect.w = (dstRect.w * rect->tCtx.fScaleX);
-    dstRect.h = (dstRect.h * rect->tCtx.fScaleY);
+    // indexing for framing. 
+    uColumns = uWidth / rect->tFr.uWidth;
+    srcRect.x = (rect->tFr.uIdx % uColumns) * rect->tFr.uWidth;
+    srcRect.y = (rect->tFr.uIdx / uColumns) * rect->tFr.uHeight;
+    srcRect.w = rect->tFr.uWidth;
+    srcRect.h = rect->tFr.uHeight;
 
-    // Convert the rotation to degrees for SDL_RenderCopyEx
+    dstRect.x = (int)rect->tCtx.fX;
+    dstRect.y = (int)rect->tCtx.fY;
+    dstRect.w = (int)(srcRect.w * rect->tCtx.fScaleX);
+    dstRect.h = (int)(srcRect.h * rect->tCtx.fScaleY);
+
     float rotationAngle = rect->tCtx.fRotation * (180.0f / M_PI);
 
     // Render the texture with the calculated position, scale, and rotation
     SDL_RenderCopyEx(
-        renderer,
+        sdlRend,
         texture,
-        NULL,            // No source rectangle (full texture)
-        &dstRect,        // Destination rectangle
-        rotationAngle,   // Rotation angle in degrees
-        NULL,            // No center (rotate around the top-left corner)
-        SDL_FLIP_NONE    // No flipping
+        &srcRect, 
+        &dstRect,     
+        rotationAngle,
+        &sdlCenter,
+        SDL_FLIP_NONE
     );
 }
 
